968f94bdc587e39e231d22ca6d026c9503f81dd7
Denis Bollo <bollodenis@gmail.com>
2025-03-25 19:14:38 +0300

    first commit

---
+++ project/script.js
@@ -0,0 +1,6 @@
+function myfunction() {
+    alert("Referees available upon request, kindly bear!");
+}
+
+let btnn = document.getElementById("btn3");
+btnn.addEventListener("click", myfunction); // No parentheses here
